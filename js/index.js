1df506e5e5ee7bc951c2633f9a9833eaaba6e826
Egeny-Myasnikov <jenyamyasnikov@gmail.com>
2022-06-01 20:56:37 +0800

    toDo

--- js/index.js
+++ js/index.js
@@ -5,51 +5,51 @@ function toDoShka() {
     const todoFormInput = todoForm.querySelector('.input__task');
     const todoTasks = document.querySelector('.todo-tasks');
     const btnClearList = document.querySelector('.btn__form__clear');
-
-
     const lst = window.localStorage;
 
-
-    function taskChanged(event) {
-
-        const id = this.closest('.task').dataset.taskId;
-
-        if (event.target.closest('.btn__complite')) {
-            const targetTask = JSON.parse(lst.getItem(id));
-            targetTask.completed ? targetTask.completed = false : targetTask.completed = true;
-            const modifiedTargetTask = JSON.stringify(targetTask);
-            lst.setItem(id, modifiedTargetTask);
+    let taskArr = lst.tasks ? JSON.parse(lst.getItem('tasks')) : [];
+    const pushTask = () => {
+        const task = {
+            text: todoFormInput.value,
+            completed: false
         }
+        taskArr.push(task);
+        todoFormInput.value = '';
+    }
 
+    const setTask = () => {
+        lst.setItem("tasks", JSON.stringify(taskArr));
+    }
 
-        reRenderItems();
+    const switchComplete = () => {
+        const btnComplete = document.querySelectorAll('.btn__complite');
+        btnComplete.forEach((btn, i) => {
+            btn.addEventListener("click", () => {
+                taskArr[i].completed = !taskArr[i].completed;
+                setTask();
+                renderTask();
+            })
+        });
+    }
+    const deleteTask = () => {
+        const btnDelete = document.querySelectorAll('.btn__delete');
+        btnDelete.forEach((btn, i) => {
+            btn.addEventListener("click", () => {
+                taskArr.splice(i, 1);
+                setTask();
+                renderTask();
+            })
+        });
     }
 
 
-
-    const getDataLst = () => {
-        let newDataObj = [];
-
-        for (let idx = 0; idx < lst.length; idx++) {
-            let key = lst.key(idx);
-            const data = lst.getItem(key);
-            const dataObj = JSON.parse(data);
-            newDataObj.push(dataObj);
-        }
-        return newDataObj;
-    }
-
-    const createTask = ({
-        id,
-        value,
-        completed
-    }) => {
-        const completedClass = `${completed === true ? ' complite': ''}`;
+    const createTask = (task, i) => {
+        const completedClass = `${task.completed === true ? ' complite': ''}`;
         return `
-    <li class="task"  data-task-id="${id}">
-        <span class="task__value ${completedClass}"> ${id + 1}) ${value} </span>
+    <li class="task">
+        <span class="task__value ${completedClass}"> ${i + 1}) ${task.text} </span>
         <span class="task__controls">
-            <button class="btn btn__form btn__complite">${completed === true ? '-': '+'}</button>
+            <button class="btn btn__form btn__complite">${task.completed === true ? '-': '+'}</button>
             <button class="btn btn__form btn__delete">
                 <svg class="svg svg-trash">
                     <use xlink:href="assets/svg/svgSprite.svg#trash"></use>
@@ -60,86 +60,43 @@ function toDoShka() {
     `
     }
 
-    const renderItems = () => {
-        const dataArr = getDataLst();
-        dataArr.forEach(data => {
-            todoTasks.innerHTML += createTask(data);
-        })
-        document.querySelectorAll('.btn').forEach(el => {
-            if (el.closest('.btn__complite')) {
-                el.addEventListener('click', taskChanged)
-            }
-        })
-        deleteTargetItem()
-    }
-
-    const reRenderItems = () => {
+    const renderTask = () => {
+        const noTasks = () => todoTasks.innerHTML = '<p>Задач нет! Дабавьте задачу</p>';
         todoTasks.innerHTML = '';
-        renderItems();
-        todoFormInput.value = '';
-    }
-
-    const addNewTask = () => {
-        todoForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            if (todoFormInput.value !== '') {
-                const todoBody = {
-                    id: lst.length,
-                    value: todoFormInput.value,
-                    completed: false,
-                }
-
-                const jsonBody = JSON.stringify(todoBody);
-
-                lst.setItem(todoBody.id, jsonBody);
-
-                reRenderItems();
-            } else {
-                alert('Напиши, пожалуйста, задачу!');
-            }
-
-
-        })
-    }
-
-    const clearTaskList = () => {
-        btnClearList.addEventListener('click', () => {
-            const question = confirm('Внимание! Отчистить ВЕСЬ список!?');
-            if (question) {
-                lst.clear();
-                reRenderItems();
-            }
-        })
-    }
-
-
-
-
-    addNewTask();
-    renderItems();
-    clearTaskList();
-
-
-    function deleteTargetItem() {
-        const btnDeleteItem = document.querySelectorAll('.btn__delete');
-
-        btnDeleteItem.forEach(btn => {
-            btn.addEventListener('click', (e) => {
-                const targetId = e.target.closest('.task').dataset.taskId;
-
-
-                const tasks = document.querySelectorAll('.task');
-
-                // tasks.forEach((task) => {
-                e.target.closest('.task').remove();
-                lst.removeItem(targetId);
-
-                // })
+        if (lst.tasks) {
+            taskArr.forEach((task, i) => {
+                todoTasks.innerHTML += createTask(task, i);
+                switchComplete();
+                deleteTask();
             })
-        })
+            if (lst.tasks.length < 4) {
+                noTasks();
+            }
+        } else {
+            noTasks();
+        }
     }
 
+    todoForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (todoFormInput.value !== '') {
+            pushTask();
+            setTask();
+            renderTask();
+        } else {
+            alert('Напишите задачу...');
+        }
+    })
+    btnClearList.addEventListener('click', () => {
+        const question = confirm('Вы уверены, что хотите полностью отчистить список задач!?');
+        if (question) {
+            lst.clear();
+            taskArr = [];
+            renderTask();
+        }
+    })
 
+    renderTask();
 }
 
 
